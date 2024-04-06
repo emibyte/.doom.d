8a5b0372f431ce039184ff3779a8f93545d3e89c
emibyte <fevymarine@gmail.com>
2024-04-06 21:11:32 +0200

    splashscreen img

--- config.el
+++ config.el
@@ -33,7 +33,10 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
+
+;; new image for dashboard
+(setq fancy-splash-image (concat doom-user-dir "splash.png"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
